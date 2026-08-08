fe3b2733e7f03bb8cb828ae4fdd752a8b7475dc9
oz10000 <ozdacat@gmail.com>
2026-08-08 18:19:32 -0300

    Update manage_position.py

--- application/use_cases/manage_position.py
+++ application/use_cases/manage_position.py
@@ -1,7 +1,13 @@
 # application/use_cases/manage_position.py
+"""
+Caso de uso: Gestión de posiciones activas.
+"""
+
 from domain.services.order_service import OrderService
 
 class ManagePosition:
+    """Gestiona una posición activa."""
+
     def __init__(self):
         self.order_service = OrderService()
 
